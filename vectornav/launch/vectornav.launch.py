f14eca125724d380b0b024fd810666683bcde81c
Alex Johnson <alexjohnson@ufl.edu>
2025-04-08 12:03:43 -0400

    remapped topic names and hid unused topics

--- vectornav/launch/vectornav.launch.py
+++ vectornav/launch/vectornav.launch.py
@@ -14,12 +14,46 @@ def generate_launch_description():
         package='vectornav', 
         executable='vectornav',
         output='screen',
+        remappings=[
+            ('/vectornav/raw/attitude', '/vectornav/_'),
+            ('/vectornav/raw/common', '/vectornav/_'),
+            ('/vectornav/raw/gps', '/vectornav/_'),
+            ('/vectornav/raw/gps2', '/vectornav/_'),
+            ('/vectornav/raw/imu', '/vectornav/_'),
+            ('/vectornav/raw/ins', '/vectornav/_'),
+            ('/vectornav/raw/time', '/vectornav/_'),
+            ('/vectornav/velocity_aiding', '/vectornav/_'),
+        ],
         parameters=[os.path.join(this_dir, 'config', 'vectornav.yaml')])
     
     start_vectornav_sensor_msgs_cmd = Node(
         package='vectornav', 
         executable='vn_sensor_msgs',
         output='screen',
+        remappings=[
+            # remap these two topics
+            ('/vectornav/imu', '/imu/data'),
+            ('/vectornav/magnetic', '/imu/mag_raw'),
+            # hide all others (unused)
+            ('/vectornav/gnss', '/vectornav/_'),
+            ('/vectornav/imu_uncompensated', '/vectornav/_'),
+            ('/vectornav/pose', '/vectornav/_'),
+            ('/vectornav/pressure', '/vectornav/_'),
+            ('/vectornav/raw/attitude', '/vectornav/_'),
+            ('/vectornav/raw/common', '/vectornav/_'),
+            ('/vectornav/raw/gps', '/vectornav/_'),
+            ('/vectornav/raw/gps2', '/vectornav/_'),
+            ('/vectornav/raw/imu', '/vectornav/_'),
+            ('/vectornav/raw/ins', '/vectornav/_'),
+            ('/vectornav/raw/time', '/vectornav/_'),
+            ('/vectornav/temperature', '/vectornav/_'),
+            ('/vectornav/time_gps', '/vectornav/_'),
+            ('/vectornav/time_pps', '/vectornav/_'),
+            ('/vectornav/time_startup', '/vectornav/_'),
+            ('/vectornav/time_syncin', '/vectornav/_'),
+            ('/vectornav/velocity_aiding', '/vectornav/_'),
+            ('/vectornav/velocity_body', '/vectornav/_'),
+        ],
         parameters=[os.path.join(this_dir, 'config', 'vectornav.yaml')])
 
     # Create the launch description and populate
